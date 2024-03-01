b2cfe398de448d3d847b5981d98f0e3c0536be9a
Виталий Беяр <vitaliy.beyar@gmail.com>
2024-03-01 13:32:34 -0330

    added  links to  header

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,25 +1,46 @@
-import Link from "next/link";
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import styles from "./links.module.css";
 import NavLink from "./navLink/navLink";
 
+const links = [
+  { title: "Homepage", path: "/" },
+  { title: "About", path: "/about" },
+  { title: "Contact", path: "/contact" },
+  { title: "Blog", path: "/blog" },
+];
+
 const Links = () => {
-  const links = [
-    { title: "Homepage", path: "/" },
-    { title: "About", path: "/about" },
-    { title: "Contact", path: "/contact" },
-    { title: "Blog", path: "/blog" },
-  ];
-  
+  const [open, setOpen] = useState(false);
+
   //TEMPORARY
   const session = true;
   const isAdmin = true;
 
   return (
-    <div className={styles.links}>
-      {links.map((link) => (
-        <NavLink item={link} key={link.title} />
-      ))}
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }} />
+        )}
+      </div>
+      <button className={styles.menuButton} onClick={()=>setOpen(prev => !prev)}>Menu</button>
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
