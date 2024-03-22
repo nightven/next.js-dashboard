ebceb6e0254878ea17ac61bc3ee3a439e3b374d1
Виталий Беяр <vitaliy.beyar@gmail.com>
2024-03-22 10:56:46 -0230

    fix

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -7,7 +7,7 @@ import { getPost } from "@/lib/data";
 // FETCH DATA WITH AN API
 const getData = async (slug) => {
   const res = await fetch(
-    `https://next-js-dashboard-bice-chi.vercel.app/${slug}`
+    `https://next-js-dashboard-bice-chi.vercel.app/api/blog/${slug}`
   );
 
   if (!res.ok) {
